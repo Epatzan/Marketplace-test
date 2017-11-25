463ee406fc9c243c3e05c455f19554fd2b364b03
Edgar Patzan <epatzan@github.com>
2017-11-25 17:09:09 -0600

    Creacion de la pagina para pidelorapido.com

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+//"use strict";
+///var $ = require('jquery');
+
+/*--------
+  Plugins
+--------*/
+/*$.fn.validate = validate;
+$(".js-sendPost").validate({
+	submitHandler : function(form){
+		form.submit();
+	}
+});*/
+
+/*$(".js-prueba").click(function(){
+	alert("guardado");
+
+});*/
